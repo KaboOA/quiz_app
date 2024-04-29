60764779d877654155ada21df009b99f1002ef5c
Ahmed Kabary <71717647+KaboOA@users.noreply.github.com>
2024-04-29 10:06:13 +0300

    first commit

---
+++ lib/start_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz_screen.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Quiz App',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22.0,
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const QuizScreen(),
+                  ),
+                );
+              },
+              color: Colors.blue,
+              textColor: Colors.white,
+              child: const Text('Start!'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
